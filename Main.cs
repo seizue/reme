92fe59e4081432b478677c29d25ff3f340c7ddad
Ven <rein.akihisa@gmail.com>
2024-01-24 11:37:11 +0800

    Added tab control

--- Main.cs
+++ Main.cs
@@ -12,9 +12,34 @@ namespace reme
 {
     public partial class Main : MetroFramework.Forms.MetroForm
     {
+      
+       
         public Main()
         {
             InitializeComponent();
+
+           userControl_Inventory1 = new UserControl_Inventory();
+           userControl_Inventory1.Visible = false; // Initially, hide the UserControl
+
+            // Add the UserControl to the main panel's controls
+            MainPanel.Controls.Add(userControl_Inventory1);
+        }
+
+        private void InventoryLabel_Click(object sender, EventArgs e)
+        { 
+               // Set the location of the UserControl within the main panel
+               userControl_Inventory1.Location = new Point(0, 0); // You may adjust the position
+               userControl_Inventory1.Visible = true;
+               userControl_Inventory1.BringToFront();
+        }
+
+        private void HomeLabel_Click(object sender, EventArgs e)
+        {
+          
+                // Set the location of the UserControl within the main panel
+                userControl_Inventory1.Location = new Point(0, 0); // You may adjust the position
+                userControl_Inventory1.Visible = false;
+          
         }
     }
 }
